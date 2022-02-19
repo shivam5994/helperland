d5f2a14ccbc7f4e75a96437c3694f472544f258d
shivam5994 <56692355+shivam5994@users.noreply.github.com>
2022-02-19 21:10:04 +0530

    Update homeController.php
    updated home controller.

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -2,32 +2,59 @@
 class homeController {
     function index(){
         
-        include 'general.php';
+        include 'common.php';
         include 'views/index.php';
     }
     function about(){
         $title='About';
-        include 'general.php';
+        include 'common.php';
         include 'views/header.php';
+        include 'views/popup-modal/login-modal.php';
         include 'views/about.php';
     }
     function faq(){
         $title='Faq';
-        include 'general.php';
+        include 'common.php';
         include 'views/header.php';
+        include 'views/popup-modal/login-modal.php';
         include 'views/faq.php';
     }
     function contact(){
         $title='Contact';
-        include 'general.php';
+        include 'common.php';
         include 'views/header.php';
+        include 'views/popup-modal/login-modal.php';
         include 'views/contact.php';
     }
     function prices(){
         $title='Prices';
-        include 'general.php';
+        include 'common.php';
         include 'views/header.php';
         include 'views/prices.php';
+    }
+
+    function customerReg(){
+        include 'common.php';
+        include 'views/Registration/customer-reg.php';
+        include 'views/footer.php';
+    }
+
+    function spReg(){
+        include 'common.php';
+        include 'views/Registration/sp-reg.php';
+    }
+
+    function changePassword(){
+        $title='Reset Password';
+        include 'common.php';
+        include 'views/header.php';
+        include 'views/popup-modal/new-password.php';
+    }
+    function bookService(){
+        $title='Book a Service';
+        include 'common.php';
+        include 'views/header.php';
+        include 'views/book-service.php';
     }
     
 }
